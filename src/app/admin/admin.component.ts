775cc0e4aff44060d31290b857e5efc4ab6ddcec
vasyldziuba <vasyl.dzuba1@gmail.com>
2020-08-23 23:59:55 +0300

    new slider added & some bugs fixes & admin page created

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+  albumName: string = '';
+
+  constructor() { }
+  urls = new Array<string>();
+  detectFiles(event) {
+    this.urls = [];
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.urls.push(e.target.result);
+        }
+        reader.readAsDataURL(file);
+      }
+    }
+
+  }
+  ngOnInit(): void {
+  }
+
+}
